6ae74a4acf6c41a2862bc6205016c252b95b0d1d
JBenavidez <jbenavidez@Mac.lan>
2025-12-13 11:30:33 -0500

    small  cleanup

--- rag-service/models/models.go
+++ rag-service/models/models.go
@@ -1,13 +1,5 @@
 package models
 
-type Document struct {
-	Text          string
-	EmbeddingText []float32
-	ProjectName   string
-	Description   string
-	Distance      float64
-}
-
 type Doc struct {
 	ID                    string
 	DateReported          string
