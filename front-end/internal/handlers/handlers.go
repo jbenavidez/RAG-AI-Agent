8af4cef14f2a138deaae0ee0530afe4728d324ef
JBenavidez <jbenavidez@Mac.lan>
2025-12-15 19:59:18 -0500

    set grpc on f.e

--- front-end/internal/handlers/handlers.go
+++ front-end/internal/handlers/handlers.go
@@ -5,6 +5,7 @@ import (
 	"frontend/internal/config"
 	"frontend/internal/models"
 	"frontend/internal/render"
+	pb "frontend/proto/generated"
 	"net/http"
 )
 
@@ -43,11 +44,29 @@ func (m *Repository) AnswerUserQuestion(w http.ResponseWriter, r *http.Request) 
 		fmt.Println(err)
 		return
 	}
+	//set grpc req
+	// req := &pb.AIAgentRequest{
+	// 	Question: payload.Question,
+	// }
+	req := &pb.AIAgentRequest{
+		Question: "List the project names along with their budget forecast and total budget changes for each interceptor sewer project.",
+	}
+	// call grcp
+	response, err := m.App.GRPCClient.GetAIAgentAnswerFromUserQuestion(r.Context(), req)
 
-	fmt.Println("the user question", payload)
+	if err != nil {
+		// set fail resposne
+		errRes := models.JSONResponse{
+			Error:   true,
+			Message: "Server Error, Unable to answer the question",
+		}
+		_ = m.writeJSON(w, http.StatusOK, errRes)
+	}
+
+	fmt.Println("Valinor_calling....", response.Answer)
 	// set response
 	agentAnwer := models.AgentResponse{
-		Answer: "hello from gondor",
+		Answer: response.Answer,
 	}
 	//set response
 	resp := models.JSONResponse{
