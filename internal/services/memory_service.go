0ca001a30c60e418ade0da4da4f2b431e9f2844a
JBenavidez <jbenavidez@Mac.lan>
2026-07-21 21:00:36 -0400

    Add new method to memoryServices

--- internal/services/memory_service.go
+++ internal/services/memory_service.go
@@ -1,9 +1,26 @@
 package services
 
-import "github.com/redis/go-redis/v9"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+const (
+	keyExpireTime = 24 * time.Hour
+)
 
 type MemoryService struct {
 	redisClient *redis.Client
+}
+
+type Chat struct {
+	Question  string    `json:"question"`
+	Answer    string    `json:"answer"`
+	createdAt time.Time `json:"created_at"`
 }
 
 func NewMemoryService(rdb *redis.Client) *MemoryService {
@@ -11,4 +28,30 @@ func NewMemoryService(rdb *redis.Client) *MemoryService {
 	return &MemoryService{
 		redisClient: rdb,
 	}
+}
+
+func (m *MemoryService) Store(ctx context.Context, sessionID, question, answer string) error {
+	// create redis key using current sessionID
+	key := fmt.Sprintf("chat:%s:messages", sessionID)
+	// create one chat turn
+	turn := Chat{
+		Question:  question,
+		Answer:    answer,
+		createdAt: time.Now(),
+	}
+	// convert chat into json
+	data, err := json.Marshal(turn)
+	if err != nil {
+		return err
+	}
+	// Save the chat turn at the end of the Redis list.
+	if err := m.redisClient.RPush(ctx, key, data).Err(); err != nil {
+		return err
+	}
+	// keep chat history for 24 hours
+	if err := m.redisClient.Expire(ctx, key, keyExpireTime).Err(); err != nil {
+		return err
+	}
+	return nil
+
 }
